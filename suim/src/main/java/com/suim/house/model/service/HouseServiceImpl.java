451985922f581d101ac985d3cced12be0b6d3b28
CHOMINSU <tntntnfkd@hansei.ac.kr>
2023-05-31 03:32:48 +0900

    Revert "2일차 2"
    This reverts commit 25c9af56f5480b6e1143934272bfef1fc037c248.

--- suim/src/main/java/com/suim/house/model/service/HouseServiceImpl.java
+++ suim/src/main/java/com/suim/house/model/service/HouseServiceImpl.java
@@ -1,14 +1,11 @@
 package com.suim.house.model.service;
 
-import java.util.ArrayList;
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.suim.house.model.dao.HouseDao;
 import com.suim.house.model.vo.House;
-import com.suim.house.model.vo.Wish;
 
 @Service
 public class HouseServiceImpl implements HouseService {
@@ -22,23 +19,5 @@ public class HouseServiceImpl implements HouseService {
 	public House selectHouse(int hno) {
 	  return houseDao.selectHouse(sqlSession, hno);
 	}
-	
-	@Override
-	public ArrayList<Wish> checkHeart(int hno) {
-	  return houseDao.checkHeart(sqlSession, hno);
-	}
-	
-    @Override
-    public void heartLike(String id, int hno) {
-        // 좋아요 기능의 구현
-        // id와 hno를 사용하여 해당 게시글에 좋아요를 추가하는 로직을 구현합니다.
-        houseDao.addHeart(sqlSession, id, hno);
-    }
 
-    @Override
-    public void heartUnlike(String id, int hno) {
-        // 좋아요 취소 기능의 구현
-        // id와 hno를 사용하여 해당 게시글의 좋아요를 취소하는 로직을 구현합니다.
-        houseDao.removeHeart(sqlSession, id, hno);
-    }
 }
